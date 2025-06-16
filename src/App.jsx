1dde36f5509fe399db20a5d3e67e67f9196ef0e7
Karthikk7293 <karthikk7293@gmail.com>
2025-06-16 21:24:48 +0530

    jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import HeaderComponent from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <div>
+        {count === 10 && <Header />}
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -17,34 +19,16 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      {count === 3 && <HeaderComponent />}
       <div className="card">
-        <Button handleClick={setCount} >
-          new count is {count}
-        </Button>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <Button handleClick={setCount} >
-          new count is {count}
-        </Button>
-        <Button handleClick={setCount} >
-          new count is {count}
-        </Button>
-        <Button handleClick={setCount} >
-          new count is {count}
-        </Button>
-        <Button handleClick={setCount} >
-          new another is {count}
-        </Button>
-        <Button handleClick={setCount} >
-          count is {count}
-        </Button>
-        <Button handleClick={setCount} >
-          new  is {count}
-        </Button>
+        {count === 8 && <HeaderComponent />}
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        {count === 4 && <HeaderComponent />}
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+    return (
+        <div className='container'>
+            <div className="section-1">
+                logo
+            </div>
+            <div className="section-2">
+                <a href="">home</a>
+                <a href="">about</a>
+            </div>
+        </div>
+    )
+}
+
+export default Header
